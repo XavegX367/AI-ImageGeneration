3e771ad56aa2898467dcc639222cb9536c7f63c5
Joris <joris367@gmail.com>
2023-01-20 20:46:06 +0100

    local api plesk

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-const host = 'http://localhost:8080/api/v1';
+// const host = 'http://localhost:8080/api/v1';
+const host = `https://api-dalle.joriskorsten.nl/api/v1`;
 
 export const generateRoute = `${host}/dalle`;
 export const handlePostRoute = `${host}/post`
